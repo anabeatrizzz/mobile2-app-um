49ebc04043c15c5f796cb4649b13d92411fc091a
anabeatrizzz <anabeatriz.augusto06@yahoo.com>
2021-01-16 20:39:13 -0300

    modificacoes

--- App.js
+++ App.js
@@ -6,76 +6,76 @@ import ponei from './assets/ponei.mp4';
 import poneiStatic from './assets/ponei-static.jpg';
 
 export default class App extends React.Component {
-	// Método construtor
-	constructor(props) {
-		super(props);
-		this.state = {
-			date: new Date().toLocaleTimeString()
-		};
-	}
+  // Método construtor
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date().toLocaleTimeString()
+    };
+  }
 
-	// Método
-	relogio(){
-		this.setState({
-			date: new Date().toLocaleTimeString()
-		});
-	}
+  // Método
+  relogio(){
+    this.setState({
+      date: new Date().toLocaleTimeString()
+    });
+  }
 
-	componentDidMount(){
-		this.timerID = setInterval(
-			() => this.relogio(),
-			1000
-		);
-	}
+  componentDidMount(){
+    this.timerID = setInterval(
+      () => this.relogio(),
+      1000
+    );
+  }
 
-	componentWillUnmount(){
-		clearInterval(this.timerID)
-	}
+  componentWillUnmount(){
+    clearInterval(this.timerID)
+  }
 
-	render(){
-		return(
-			<View style={estilos.container}>
-				<Text style={estilos.texto}>Relógio ⏰</Text>
-				<Text>{'\n'}</Text>
-				<Text style={estilos.texto}>{this.state.date}</Text>
-				<View style={estilos.videoStyle}>
-				<Video
-					source={ponei}
-					style={{ width: 200, height: 200 }}
-					rate={1.0}
-					volume={1.0}
-					useNativeControls
-					isMuted={false}
-					resizeMode="cover"
-					isLooping
-					posterSource={poneiStatic}
-					posterStyle={{ width: 200, height: 200 }}
-					usePoster
-				/>
-				</View>
-			</View>
-		)
-	}
+  render(){
+    return(
+      <View style={estilos.container}>
+        <Text style={estilos.texto}>Relógio ⏰</Text>
+        <Text>{'\n'}</Text>
+        <Text style={estilos.texto}>{this.state.date}</Text>
+        <View style={estilos.videoStyle}>
+        <Video
+          source={ponei}
+          style={{ width: 200, height: 200 }}
+          rate={1.0}
+          volume={1.0}
+          useNativeControls
+          isMuted={false}
+          resizeMode="cover"
+          isLooping
+          posterSource={poneiStatic}
+          posterStyle={{ width: 200, height: 200 }}
+          usePoster
+        />
+        </View>
+      </View>
+    )
+  }
 }
 
 const estilos = StyleSheet.create({
-	container: {
-		flex:  1,
-		flexDirection: 'column',
-		alignItems: 'center',
-		justifyContent: 'center'
-	},
-	texto: {
-		fontSize: 60,
-		width: '100%',
-		textAlign: 'center'
-	},
+  container: {
+    flex:  1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  texto: {
+    fontSize: 60,
+    width: '100%',
+    textAlign: 'center'
+  },
 
-	videoStyle: {
-		width: '100%',
-		justifyContent: 'center',
-		alignItems: 'center',
-		position: 'absolute',
-		bottom: 1
-	}
+  videoStyle: {
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    bottom: 1
+  }
 })
